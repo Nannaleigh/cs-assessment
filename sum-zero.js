237892cfd948e7ed291afd50baef8e0a44acb92e
Nannaleigh <tatianna.martinez71@gmail.com>
2022-02-18 10:49:49 -0700

    Finished problems 1 - 3 on part 2

---
+++ sum-zero.js
@@ -0,0 +1,16 @@
+function sumZero(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for(let j=0; j < arr.length; j++ ) {
+            if (arr[i] + arr[j] === 0) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+console.log(sumZero([1, 2, 3, 5, -2]));
+
+// time complexity : O(n^2)
+//space complexity : O(n)
+
